80aa4b8aec62ac8e17d666df338d558695e302ae
Scott Martin <scott+school@scottmartin.systems>
2018-02-07 15:32:28 +0000

    Drop notion of locally stored currency list, hardcode subset of fiat currencies recognized by converter

--- site.js
+++ site.js
@@ -1,64 +1,87 @@
 let debug = 1;
+
 showCurrencyList();
+
+function showCurrencyList () {
+	let fiatList = document.createElement('dl');
+
+	let currencyLists = loadCurrencyLists();
+	let fiatCurrencies = currencyLists.fiatCurrencies;
+
+	Object.keys(fiatCurrencies).forEach((currency) => {
+		let currencyCode = document.createElement('dt');
+		currencyCode.textContent = currency;
+		fiatList.appendChild(currencyCode);
+
+		let currencyName = document.createElement('dd');
+		currencyName.textContent = fiatCurrencies[currency];
+		fiatList.appendChild(currencyName);
+	})
+
+	document.getElementById('currency-list-container').appendChild(fiatList);
+}
 
 function loadCurrencyLists () {
 	let currencyLists = {};
 
-	if (localStorage.cryptoCurrency) {
-		if (debug) console.log('Loading cryptocurrency list from localStorage');
-	} else {
-		if (debug) console.log('Loading cryptocurrency list from file');
-
-		scriptLoader('cryptoCurrencies.js');
-		localStorage.setItem('cryptoCurrencyData', cryptoCurrencyData);
-	}
-
-	if (localStorage.fiatCurrency) {
-		if (debug) console.log('Loading fiat currency list from localStorage');
-	} else {
-		if (debug) console.log('Loading fiat currency list from file');
-
-		scriptLoader('fiatCurrencies.js');
-		localStorage.setItem('fiatCurrencyData', fiatCurrencyData);
-	}
-
-	currencyLists.cryptoCurrencies = JSON.parse(localStorage.cryptoCurrencyData);
-	currencyLists.fiatCurrencies = JSON.parse(localStorage.fiatCurrencyData);
+	currencyLists.fiatCurrencies = {
+		'AED' : 'United Arab Emirates dirham',
+		'ARS' : 'Argentine peso',
+		'AUD' : 'Australian dollar',
+		'BAM' : 'Bosnia and Herzegovina convertible mark',
+		'BGN' : 'Bulgarian lev',
+		'BRL' : 'Brazilian real',
+		'CAD' : 'Canadian dollar',
+		'CHF' : 'Swiss franc',
+		'CLP' : 'Chilean peso',
+		'CNY' : 'Chinese yuan',
+		'COP' : 'Colombian peso',
+		'CZK' : 'Czech koruna',
+		'DKK' : 'Danish krone',
+		'DOP' : 'Dominican peso',
+		'EUR' : 'Euro',
+		'GBP' : 'Pound sterling',
+		'HKD' : 'Hong Kong dollar',
+		'HRK' : 'Croatian kuna',
+		'HUF' : 'Hungarian forint',
+		'IDR' : 'Indonesian rupiah',
+		'ILS' : 'Israeli new shekel',
+		'INR' : 'Indian rupee',
+		'IRR' : 'Iranian rial',
+		'JPY' : 'Japanese yen',
+		'KES' : 'Kenyan shilling',
+		'KRW' : 'South Korean won',
+		'MXN' : 'Mexican peso',
+		'MXV' : 'Mexican Unidad de Inversion',
+		'MYR' : 'Malaysian ringgit',
+		'NGN' : 'Nigerian naira',
+		'NOK' : 'Norwegian krone',
+		'NZD' : 'New Zealand dollar',
+		'PAB' : 'Panamanian balboa',
+		'PEN' : 'Peruvian Sol',
+		'PKR' : 'Pakistani rupee',
+		'PLN' : 'Polish złoty',
+		'RON' : 'Romanian leu',
+		'RUB' : 'Russian ruble',
+		'SAR' : 'Saudi riyal',
+		'SEK' : 'Swedish krona',
+		'SGD' : 'Singapore dollar',
+		'THB' : 'Thai baht',
+		'TRY' : 'Turkish lira',
+		'UAH' : 'Ukrainian hryvnia',
+		'UGX' : 'Ugandan shilling',
+		'USD' : 'United States dollar',
+		'VEF' : 'Venezuelan bolívar',
+		'VND' : 'Vietnamese đồng',
+		'XAF' : 'Central African CFA franc',
+		'XAG' : 'Silver',
+		'XAU' : 'Gold',
+		'XBA' : 'European Composite Unit',
+		'XBB' : 'European Monetary Unit',
+		'XBC' : 'European Unit of Account 9',
+		'ZAR' : 'South African rand',
+		'ZMW' : 'Zambian kwacha'
+	};
 
 	return currencyLists;
-}
-
-function currencyListLoader (list) {
-	if (localStorage[list]) {
-		if (debug) console.log(`Loading ${list} list from localStorage`);
-	} else {
-		if (debug) console.log(`Loading ${list} list from file`);
-
-		scriptLoader(`${list}.js`);
-		localStorage.setItem(`${list}Data`, cryptoCurrencyData);
-	}
-}
-
-function scriptLoader (script) {
-	let head = document.getElementsByTagName('head')[0];
-	let loader = document.createElement('script');
-	loader.type = 'text/javascript';
-	loader.src = script;
-	head.appendChild(loader);
-}
-
-function showCurrencyList () {
-	let cryptoList = document.createElement('ul');
-
-	let currencyLists = loadCurrencyLists();
-	// let cryptoCurrencies = currencyLists.cryptoCurrencies;
-
-	// Object.keys(cryptoCurrencies).forEach((currency) => {
-	// 	let currencyItem = document.createElement('li');
-
-	// 	currencyItem.textContent = cryptoCurrencies[currency].nameAndSymbol;
-	// 	cryptoList.appendChild(currencyItem);
-	// })
-
-	// document.getElementById('currency-list-container').appendChild(cryptoList);
 }
